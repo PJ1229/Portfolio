a93d4e007aa13a39e2c05e07aad46aa24c96d8ce
PJ <peterjoe2005@gmail.com>
2025-01-31 00:34:55 -0500

    setting up server

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve your index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
